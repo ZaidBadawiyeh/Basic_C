49bf176e1a6d508b15577cd4f13c7e7d781ba419
Zaid Badawiyeh <zaid@badawiyeh.net>
2024-03-03 21:23:10 -0700

    add capitalization function script

---
+++ capitilization.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   string start
+    cout<<"please enter a string"<< endl;
+    getLine (cin, start);
+    for(int i=0, i < start, length(); i++ )
+    {
+        if (start[i]>97 && start[i]<=122)
+        {
+            char upper
+            upper = start[i] -32;
+            cout>> upper;
+            //add to output
+        }
+        else
+        {
+            cout << start[i];
+        }
+    }
+   
+
+}
